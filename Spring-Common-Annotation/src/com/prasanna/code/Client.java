8ee68c8eeb25055da5cd3c8340de2ccc20d85a3c
PrasannaThilakarathne <prasanna.srilal.7@gmail.com>
2020-07-28 23:41:08 +0530

    create bean without xml file

--- Spring-Common-Annotation/src/com/prasanna/code/Client.java
+++ Spring-Common-Annotation/src/com/prasanna/code/Client.java
@@ -1,13 +1,12 @@
 package com.prasanna.code;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
-		System.out.println("Bean file loaded");
+		ApplicationContext context = new AnnotationConfigApplicationContext(CollageConfig.class);
 		Collage collage = context.getBean("collageBean" , Collage.class);
 		System.out.println("The collage object created by spring is : "+collage);
 
---
+++ Spring-Common-Annotation/src/com/prasanna/code/CollageConfig.java
@@ -0,0 +1,10 @@
+package com.prasanna.code;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.prasanna.code")
+public class CollageConfig {
+
+}
